27086ecae3195ad7423857936b14ef0377bfd105
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-06-19 21:13:33 +0530

    Added Styles.js

--- bank-modern-app/src/App.jsx
+++ bank-modern-app/src/App.jsx
@@ -1,9 +1,12 @@
-import React from "react"
+//import React from "react"
+import styles from './style'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}></div>
+      </div>
+    )
 }
 
 export default App

3bf7c0ff86bfc7577104219b640906a1759ee16d
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-06-20 23:25:58 +0530

    Added Components

--- bank-modern-app/src/App.jsx
+++ bank-modern-app/src/App.jsx
@@ -4,7 +4,20 @@ import styles from './style'
 const App = () => {
   return (
       <div className="bg-primary w-full overflow-hidden">
-        <div className={`${styles.paddingX} ${styles.flexCenter}`}></div>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className="{`${styles.boxWidth}`} ">
+            Navbar
+          </div>
+        </div>
+        <div className="bg-primary ${styles.flexStart}`">
+          <div className='{`${styles.boxWidth}`}'>
+          Stats
+          Business
+          
+          </div>
+        </div>
+
+
       </div>
     )
 }
